320acbfd42345ab2d2e2668ab7a65f5a6a917f69
gowrishdba <gowrishdba@yahoo.com>
2015-05-12 21:54:22 +0530

    Create list_pdbs.sql

---
+++ list_pdbs.sql
@@ -0,0 +1,8 @@
+set pages 100
+column pdb_name format a30
+
+prompt List from dba_pdbs
+select pdb_id, pdb_name, con_id from dba_pdbs order by 1,3,2;
+
+prompt List from v$pdbs with open-mode info
+select con_id, name, open_mode from v$pdbs order by 1,2;

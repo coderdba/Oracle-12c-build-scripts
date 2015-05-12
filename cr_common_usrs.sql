c2d8aa4aa597cf5cd28088894c23b682f100bb0d
gowrishdba <gowrishdba@yahoo.com>
2015-05-12 18:53:55 +0530

    Create cr_common_usrs.sql

---
+++ cr_common_usrs.sql
@@ -0,0 +1,9 @@
+--
+-- Create common users
+--
+--
+-- Help: http://oracle-base.com/articles/12c/multitenant-manage-users-and-privileges-for-cdb-and-pdb-12cr1.php#create-common-users
+--
+
+create user c##user1 identified by user#1234;
+-- container=all need not be specified 

618352f53ac07e97dfc2246e0434ec52e798a90f
gowrishdba <gowrishdba@yahoo.com>
2015-05-12 19:25:49 +0530

    Update cr_common_users.sql

--- cr_common_users.sql
+++ cr_common_users.sql
@@ -7,3 +7,14 @@
 
 create user c##user1 identified by user#1234;
 -- container=all need not be specified 
+
+grant connect to c##user1 container=all;
+-- container=all is important here, otherwise cannot connect to PDBs using this user
+
+-- Give privilege specific to a PDB
+alter session set container = PDB1;
+grant select_catalog_role to c##user1;
+
+-- Verify which role/privilege has been provided to this user in each cdb/pdb
+select * from cdb_role_privs where grantee='C##USER1';
+select * from cdb_sys_privs where grantee='C##USER1';

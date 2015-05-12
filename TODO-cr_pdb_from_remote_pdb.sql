7b687a5c49bb473fe857aa35f7b61d010cce12e8
gowrishdba <gowrishdba@yahoo.com>
2015-05-12 22:13:20 +0530

    Create TODO-cr_pdb_from_remote_pdb.sql

---
+++ TODO-cr_pdb_from_remote_pdb.sql
@@ -0,0 +1,4 @@
+-- http://docs.oracle.com/database/121/ADMIN/cdb_plug.htm#ADMIN13593
+-- see section Cloning a Remote PDB or Non-CDB
+
+-- http://www.oracle.com/webfolder/technetwork/tutorials/obe/db/12c/r1/pdb/pdb_clone/pdb_clone.html

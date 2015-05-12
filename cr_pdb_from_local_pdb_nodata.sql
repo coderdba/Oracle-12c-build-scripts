ff3aa890b17cb780c8aed84d3946037fbc68610e
gowrishdba <gowrishdba@yahoo.com>
2015-05-12 22:20:20 +0530

    Update cr_pdb_from_local_pdb_nodata.sql

--- cr_pdb_from_local_pdb_nodata.sql
+++ cr_pdb_from_local_pdb_nodata.sql
@@ -1,5 +1,19 @@
  --
  --
  --
- 
- CREATE PLUGGABLE DATABASE PDB2 from PDB1 NO DATA;
+
+-- Prepare source PDB to provide consistent data by open-read-only
+alter pluggable database pdb1 close immediate instances=all;
+alter pluggable database pdb1 open read only instances=all;
+
+-- Clone the PDB
+CREATE PLUGGABLE DATABASE pdb2 FROM pdb1 NO DATA;
+alter pluggable database pdb2 open instances=all;
+
+-- Re-open source pdb in read-write mode
+alter pluggable database pdb1 close immediate instances=all;
+alter pluggable database pdb1 open instances=all;
+
+-- Verify
+select con_id, name, open_mode from v$pdbs order by 1,2;
+

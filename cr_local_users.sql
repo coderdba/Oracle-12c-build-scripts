f0178b90145d264eb36a7c6ba67eea13b53bafc3
gowrishdba <gowrishdba@yahoo.com>
2015-05-12 23:16:20 +0530

    Update cr_local_users.sql

--- cr_local_users.sql
+++ cr_local_users.sql
@@ -10,7 +10,12 @@ create role perfdba_role;
 
 -- Create users
 -- NOTE - the initial profile will be 'DEFAULT' which is created at the container level already
-create user dbaquery identified by dbaquery ;
+--        However, if the local default profile does not have a password verify function associated with it
+--        then we can use a common profile that has a password verify function associated with it 
+--        The create-user statement below uses a common profile
+--        ALTERNATIVELY - Create a local verify-function and associate it with the local default profile
+--
+create user dbaquery identified by dbaquery profile c##common_profile;
 select username, default_tablespace, common from cdb_users where con_id=3 and username ='DBAQUERY';
 alter user dbaquery default tablespace dbats;
 select username, default_tablespace, common from cdb_users where con_id=3 and username ='DBAQUERY';

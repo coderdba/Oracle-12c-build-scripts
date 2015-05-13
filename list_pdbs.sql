9f174c2f7859546624dc5ad8214c440c386777fa
gowrishdba <gowrishdba@yahoo.com>
2015-05-13 14:57:12 +0530

    Update list_pdbs.sql

--- list_pdbs.sql
+++ list_pdbs.sql
@@ -7,5 +7,5 @@ show pdbs
 prompt List from dba_pdbs
 select pdb_id, pdb_name, con_id from dba_pdbs order by 1,3,2;
 
-prompt List from v$pdbs with open-mode info
-select con_id, name, open_mode from v$pdbs order by 1,2;
+prompt List from gv$pdbs with open-mode info
+select con_id, name, inst_id, open_mode from gv$pdbs order by 1,2,3

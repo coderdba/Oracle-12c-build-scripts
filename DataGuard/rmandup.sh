2600d787cc634c01e89ded70ff50881348960087
Coder <coderdba@users.noreply.github.com>
2017-01-30 11:41:45 +0530

    Delete rmandup.sh

--- DataGuard/rmandup.sh
+++
@@ -1,4 +0,0 @@
-#-- DBUNIQUENAME_PRIM - Primary unique name
-#-- DBUNIQUENAME_STBY  - Standby unique name
-
- nohup rman target sys/lhfRL4DB1@rcll04-vip1/DBUNIQUENAME_PRIM auxiliary sys/lhfRL4DB1@rcll05-vip1/DBUNIQUENAME_STBY @rmanduplicateRL4DB1.cmd &
